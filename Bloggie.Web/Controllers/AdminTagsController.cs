3f90a0e39f55b7d6a3a4d61d5ca05b443b9c09fa
Borte Avsaroglu <borteavsaroglu@hotmail.com>
2023-08-23 16:48:49 +0300

    For the Tags adding created controller, view model and view classes.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,24 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+
+            return View("Add");
+        }
+    }
+}
